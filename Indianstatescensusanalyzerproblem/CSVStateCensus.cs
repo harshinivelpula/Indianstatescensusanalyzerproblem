d815eaec3593031fa1e39a81e76c67f94ac99cc1
DELL <105187636+harshinivelpula@users.noreply.github.com>
2023-01-04 13:46:05 +0530

    [charu] Reading data from csv file

---
+++ Indianstatescensusanalyzerproblem/CSVStateCensus.cs
@@ -0,0 +1,24 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indianstatescensusanalyzerproblem
+{
+    public class CSVStateCensus
+    {
+        public void ReadStateCensusData(string filepath)
+        {
+            using var reader = new StreamReader(filepath);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var records = csv.GetRecords<StateCensusData>().ToList();
+            foreach (var data in records)
+            {
+                Console.WriteLine(data.State + " " + data.Population + " " + data.AreaInSqKm + " " + data.DensityPerSqKm + " ");
+            }
+        }
+    }
+}
---
+++ Indianstatescensusanalyzerproblem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Indianstatescensusanalyzerproblem
+{
+    class Program
+    {
+        public static string stateCensusCSVFilePath = @"C:\Users\DELL\source\repos\Indianstatescensusanalyzerproblem\Indianstatescensusanalyzerproblem\files\StateCensusData.csv";
+        public static void Main(string[] args)
+        {
+            CSVStateCensus cSVStateCensus = new CSVStateCensus();
+            cSVStateCensus.ReadStateCensusData(stateCensusCSVFilePath);
+        }
+    }
+}
