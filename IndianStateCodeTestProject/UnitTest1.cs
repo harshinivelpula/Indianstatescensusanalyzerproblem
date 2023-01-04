89f9a3d0352cabe9d464097ed8d88e8b740507bb
DELL <105187636+harshinivelpula@users.noreply.github.com>
2023-01-04 16:24:11 +0530

    [charu]given statecensus file if incorrect returns customexception

--- IndianStateCodeTestProject/UnitTest1.cs
+++ IndianStateCodeTestProject/UnitTest1.cs
@@ -6,6 +6,8 @@ namespace IndianStateCodeTestProject
     {
         public static string stateCodeCSVFilePath = @"C:\Users\DELL\source\repos\Indianstatescensusanalyzerproblem\Indianstatescensusanalyzerproblem\files\StateCode.csv";
         public static string stateCodeIncorrectCSVFilePath = @"C:\Users\DELL\source\repos\Indianstatescensusanalyzerproblem\Indianstatescensusanalyzerproblem\files\StateCodedata.csv";
+        public static string stateCodeIncorrectCSVFileType = @"C:\Users\DELL\source\repos\Indianstatescensusanalyzerproblem\Indianstatescensusanalyzerproblem\files\StateCodedata.txt";
+
 
         public void GivenStateCodeData_WhenAnalyse_ShoulReturnNumberOfRecordMatches()
         {
@@ -26,5 +28,18 @@ namespace IndianStateCodeTestProject
                 Assert.AreEqual(ex.Message, "Incorrect FilePath");
             }
         }
+        [Test]
+        public void GivenStateCodeDataFileTypeIncorrect_WhenAnalyzed_ShouldReturnException()
+        {
+            StateCodeAnalyser stateCodeAnalyser = new StateCodeAnalyser();
+            try
+            {
+                int record = stateCodeAnalyser.ReadStateCensusData(stateCodeIncorrectCSVFilePath);
+            }
+            catch (StateCensusAndCodeException ex)
+            {
+                Assert.AreEqual(ex.Message, "Incorrect FilePath");
+            }
+        }
     }
-} 
+}
